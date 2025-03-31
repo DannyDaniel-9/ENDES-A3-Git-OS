aec84b249b53ee57658ce588af160c377fd47b1a
DannyDaniel-9 <3dldadaniel26@gmail.com>
2025-03-31 11:58:24 +0200

    Segundo commit

--- DDDA.java
+++ DDDA.java
@@ -3,6 +3,8 @@ public class DDDA {
   public static void main(String[] args) {
 
      System.out.println("Creacion de clase Java desde GitBash") 
+
+     System.out.println("Modificacion para segundo commit")
   }
 }
 
