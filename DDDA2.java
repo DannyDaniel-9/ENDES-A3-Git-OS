a456d58481e51b3d2ba31b6b857430132507ffef
HowardP98 <howardpadron98@gmail.com>
2025-04-02 22:54:35 +0200

    Segundo commmit de Howard

--- DDDA2.java
+++ DDDA2.java
@@ -1,5 +1,6 @@
 public class DDDA2 {
     public static void main(String[] args) {
         System.out.println("Soy la clase DDDA2");
+	System.out.println("Segundo comentario modificado de Howard");
     }
 }
